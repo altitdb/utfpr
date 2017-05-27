ddcba6e17936e7e56b5f2a0d951b9375afa8af63
Altieres de Matos <altitdb@gmail.com>
2017-05-26 23:07:29 -0300

    Adicionando comentários

--- vts/trabalho-vts/src/test/java/br/edu/utfpr/exercicio02/ClassificadorTest.java
+++ vts/trabalho-vts/src/test/java/br/edu/utfpr/exercicio02/ClassificadorTest.java
@@ -5,51 +5,122 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ClassificadorTest {
-	
+
+	/**
+	 * Defino a classe que será testada unitáriamente.
+	 */
 	private Classificador classificador;
-	
+
+	/**
+	 * Antes da execução de cada método criamos sempre uma nova instância da
+	 * classe a ser testada, com intuido de gerar problemas no teste devido a
+	 * "sujeira" deixada por outros testes.
+	 */
 	@Before
 	public void setup() {
 		classificador = new Classificador();
 	}
 
+	/**
+	 * Com este caso de teste validamos se ao passar uma idade inválida, ou
+	 * seja, menor que zero, temos uma rejeição do valor. Caso o valor seja
+	 * rejeitado, receberemos uma exception do tipo IllegalArgumentException. Se
+	 * uma exception do tipo IllegalArgumentException não for lançada pelo
+	 * método que estamos testando o teste falhará.
+	 * 
+	 * (i) um valor de idade inválido;
+	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void deveriaValidarIdadeInvalidaMenorQueZero() {
+		// Arrange
 		Pessoa pessoa = new Pessoa("Altieres", -1);
+		// Act
 		classificador.definirFaixaEtaria(pessoa);
+		// Assert
+		Assert.fail("Não deveria chegar até aqui");
 	}
-	
+
+	/**
+	 * Com este caso de teste validamos se ao passar uma idade inválida, ou
+	 * seja, maior que cento e dez, temos uma rejeição do valor. Caso o valor
+	 * seja rejeitado, receberemos uma exception do tipo
+	 * IllegalArgumentException. Se uma exception do tipo
+	 * IllegalArgumentException não for lançada pelo método que estamos testando
+	 * o teste falhará.
+	 * 
+	 * (i) um valor de idade inválido;
+	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void deveriaValidarIdadeInvalidaMaiorQueCentoEDez() {
+		// Arrange
 		Pessoa pessoa = new Pessoa("Altieres", 111);
+		// Act
 		classificador.definirFaixaEtaria(pessoa);
+		// Assert
+		Assert.fail("Não deveria chegar até aqui");
 	}
-	
+
+	/**
+	 * Neste caso de teste informamos uma idade válida para faixa etária de
+	 * crianças e validamos se a mesma esta de acordo.
+	 * 
+	 * (ii) uma pessoa que é criança;
+	 */
 	@Test
 	public void deveriaRetornarPessoaDoTipoCrianca() {
+		// Arrange
 		Pessoa pessoa = new Pessoa("Altieres", 8);
+		// Act
 		String faixaEtaria = classificador.definirFaixaEtaria(pessoa);
+		// Assert
 		Assert.assertEquals("Altieres eh crianca", faixaEtaria);
 	}
-	
+
+	/**
+	 * Neste caso de teste informamos uma idade válida para faixa etária de
+	 * adolescentes e validamos se a mesma esta de acordo.
+	 * 
+	 * (iii) uma pessoa que é adolescente;
+	 */
 	@Test
 	public void deveriaRetornarPessoaDoTipoAdolescente() {
+		// Arrange
 		Pessoa pessoa = new Pessoa("Altieres", 15);
+		// Act
 		String faixaEtaria = classificador.definirFaixaEtaria(pessoa);
+		// Assert
 		Assert.assertEquals("Altieres eh adolescente", faixaEtaria);
 	}
-	
+
+	/**
+	 * Neste caso de teste informamos uma idade válida para faixa etária de
+	 * adultos e validamos se a mesma esta de acordo.
+	 * 
+	 * (iv) uma pessoa que é adulta;
+	 */
 	@Test
 	public void deveriaRetornarPessoaDoTipoAdulto() {
+		// Arrange
 		Pessoa pessoa = new Pessoa("Altieres", 39);
+		// Act
 		String faixaEtaria = classificador.definirFaixaEtaria(pessoa);
+		// Assert
 		Assert.assertEquals("Altieres eh adulto", faixaEtaria);
 	}
-	
+
+	/**
+	 * Neste caso de teste informamos uma idade válida para faixa etária de
+	 * idosos e validamos se a mesma esta de acordo.
+	 * 
+	 * (ii) uma pessoa que é idosa.
+	 */
 	@Test
 	public void deveriaRetornarPessoaDoTipoIdoso() {
+		// Arrange
 		Pessoa pessoa = new Pessoa("Altieres", 60);
+		// Act
 		String faixaEtaria = classificador.definirFaixaEtaria(pessoa);
+		// Assert
 		Assert.assertEquals("Altieres eh idoso", faixaEtaria);
 	}
 
--- vts/trabalho-vts/src/test/java/br/edu/utfpr/exercicio03/RelatorioDeFuncionariosTest.java
+++ vts/trabalho-vts/src/test/java/br/edu/utfpr/exercicio03/RelatorioDeFuncionariosTest.java
@@ -8,14 +8,27 @@ import org.mockito.Mockito;
 
 public class RelatorioDeFuncionariosTest {
 
+	/**
+	 * Defino a classe que será testada unitáriamente.
+	 */
 	private RelatorioDeFuncionarios relatorioDeFuncionarios;
-	
+
+	/**
+	 * Caso de teste: Existem 2 funcionários na categoria "tecnico" que não
+	 * estão com o CPF bloqueado.
+	 */
 	@Test
 	public void casoDeTeste01() {
+		// Arrange
+		// Crio um mock da interface FuncionarioDAO visto que não tenho nenhuma
+		// implementação dela
 		FuncionarioDAO funcionarioDao = Mockito.mock(FuncionarioDAO.class);
+		// Crio uma lista de funcionários que serão utilizados pelo mock
 		ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();
+		// Crio a funcionária Mariana
 		Funcionario mariana = new Funcionario();
 		mariana.setCpf("111111111-11");
+		// Adiciono a funcionária mariana na lista de funcionários
 		funcionarios.add(mariana);
 		Funcionario larissa = new Funcionario();
 		larissa.setCpf("222222222-22");
@@ -23,21 +36,46 @@ public class RelatorioDeFuncionariosTest {
 		Funcionario glauco = new Funcionario();
 		glauco.setCpf("333333333-33");
 		funcionarios.add(glauco);
+		// Configuro o mock da interface FuncionarioDAO para retornar a lista de
+		// funcionários quando o método getFuncionariosBy for invocado com o
+		// parâmetro tecnico
 		Mockito.when(funcionarioDao.getFuncionariosBy("tecnico")).thenReturn(funcionarios);
+		// Crio uma instância da classe que será testada unitáriamente passando
+		// o mock da interface FuncionarioDAO no construtor
 		relatorioDeFuncionarios = new RelatorioDeFuncionarios(funcionarioDao);
-		
+		// Crio um mock da interface ReceitaFederal visto que não tenho nenhuma
+		// implementação dela
 		ReceitaFederal receitaFederal = Mockito.mock(ReceitaFederal.class);
+		// Informo para classe RelatorioDeFuncionarios que utilizará o mock
+		// receitaFederal
 		relatorioDeFuncionarios.setRf(receitaFederal);
+		// Configuro o mock da itnerface ReceitaFederal para retornar o valor
+		// true quando o método isCPFBloqueado for invocado com o parâmetro
+		// 111111111-11
 		Mockito.when(receitaFederal.isCPFBloqueado("111111111-11")).thenReturn(true);
 		Mockito.when(receitaFederal.isCPFBloqueado("222222222-22")).thenReturn(true);
-		
+
+		// Act
+		// Realizo a chamada do método getFuncComCPFBloqueado passando como
+		// parâmetro o valor tecnico, e armazeno o valor do retorno do método na
+		// variável funcionariosComCPFBloqueado
 		int funcionariosComCPFBloqueado = relatorioDeFuncionarios.getFuncComCPFBloqueado("tecnico");
+
+		// Assert
+		// Defino a quantidade de funcionários com CPF bloqueados eu espero
+		// receber após ter invocado o método getFuncComCPFBloqueado
 		int valorExperado = 2;
-		Assert.assertEquals(valorExperado , funcionariosComCPFBloqueado);
+		// Verifico se o valor retornado é igual ao valor que estou esperando
+		Assert.assertEquals(valorExperado, funcionariosComCPFBloqueado);
 	}
-	
+
+	/**
+	 * Caso de teste: Existe 1 funcionário na categoria "analista" que está com
+	 * o CPF bloqueado.
+	 */
 	@Test
 	public void casoDeTeste02() {
+		// Arrange
 		FuncionarioDAO funcionarioDao = Mockito.mock(FuncionarioDAO.class);
 		ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();
 		Funcionario julio = new Funcionario();
@@ -48,18 +86,27 @@ public class RelatorioDeFuncionariosTest {
 		funcionarios.add(lenon);
 		Mockito.when(funcionarioDao.getFuncionariosBy("analista")).thenReturn(funcionarios);
 		relatorioDeFuncionarios = new RelatorioDeFuncionarios(funcionarioDao);
-		
+
 		ReceitaFederal receitaFederal = Mockito.mock(ReceitaFederal.class);
 		relatorioDeFuncionarios.setRf(receitaFederal);
 		Mockito.when(receitaFederal.isCPFBloqueado("444444444-44")).thenReturn(true);
-		
+
+		// Act
 		int funcionariosComCPFBloqueado = relatorioDeFuncionarios.getFuncComCPFBloqueado("analista");
+
+		// Assert
 		int valorExperado = 1;
-		Assert.assertEquals(valorExperado , funcionariosComCPFBloqueado);
+		Assert.assertEquals(valorExperado, funcionariosComCPFBloqueado);
 	}
-	
+
+	/**
+	 * Caso de teste: Existem 4 funcionários na categoria "gerente" com os CPFs:
+	 * (123456789-00, 111222333-44, 654321987-23, 098876654-99), sendo que os
+	 * CPFs 111222333-44 e 098876654-99 estão bloqueados.
+	 */
 	@Test
 	public void casoDeTeste03() {
+		// Arrange
 		FuncionarioDAO funcionarioDao = Mockito.mock(FuncionarioDAO.class);
 		ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();
 		Funcionario claudia = new Funcionario();
@@ -76,15 +123,18 @@ public class RelatorioDeFuncionariosTest {
 		funcionarios.add(kaique);
 		Mockito.when(funcionarioDao.getFuncionariosBy("gerente")).thenReturn(funcionarios);
 		relatorioDeFuncionarios = new RelatorioDeFuncionarios(funcionarioDao);
-		
+
 		ReceitaFederal receitaFederal = Mockito.mock(ReceitaFederal.class);
 		relatorioDeFuncionarios.setRf(receitaFederal);
 		Mockito.when(receitaFederal.isCPFBloqueado("111222333-44")).thenReturn(true);
 		Mockito.when(receitaFederal.isCPFBloqueado("098876654-99")).thenReturn(true);
-		
+
+		// Act
 		int funcionariosComCPFBloqueado = relatorioDeFuncionarios.getFuncComCPFBloqueado("gerente");
+
+		// Assert
 		int valorExperado = 2;
-		Assert.assertEquals(valorExperado , funcionariosComCPFBloqueado);
+		Assert.assertEquals(valorExperado, funcionariosComCPFBloqueado);
 	}
 
 }
