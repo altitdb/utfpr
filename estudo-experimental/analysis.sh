13cda7196ee3c6006898d1a80099f5908a224073
Altieres de Matos <almatos@uolinc.com>
2020-08-15 16:53:44 -0300

    Adicionando script de analise

---
+++ estudo-experimental/analysis.sh
@@ -0,0 +1,14 @@
+cd ~/Desktop/estudo-de-caso/com-criterios/participante01/bowling-game-microservice
+mvn clean install sonar:sonar
+
+cd ~/Desktop/estudo-de-caso/com-criterios/participante02/bowling-game-microservice
+mvn clean install sonar:sonar
+
+cd ~/Desktop/estudo-de-caso/com-criterios/participante03/bowling-game-microservice
+mvn clean install sonar:sonar
+
+cd ~/Desktop/estudo-de-caso/sem-criterios/participante01/bowling-game-microservice
+mvn clean install sonar:sonar
+
+cd ~/Desktop/estudo-de-caso/sem-criterios/participante02/bowling-game-microservice
+mvn clean install sonar:sonar
